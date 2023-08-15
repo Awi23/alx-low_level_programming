27b58fdb5647ca35972623c0660d47d7ddfc629f
Awelani <mphohoniawelani8@gmail.com>
2023-08-15 14:19:13 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+    char curr;
+    for(curr = 'a'; curr <= 'z'; curr ++){
+        _putchar(curr);
+    }
+    _putchar('\n');
+} 
+
